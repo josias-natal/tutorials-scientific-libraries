30e626f6861bea6634d305941cca9a8e2575ebba
brunocampos01 <brunocampos01@gmail.com>
2018-09-03 01:04:29 -0300

    numpy and other updates

---
+++ scikit_learn/playgroud_sklearn.py
@@ -0,0 +1,17 @@
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+
+# training data
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+
+
+clf = GaussianNB() # make a classifier
+clf.fit(X, Y) # gives the classifier training data (features, labels)
+print(clf.predict([[-0.8, -1]])) # what do you think this point is: class 1 or 2?
+
+clf_pf = GaussianNB()
+clf_pf.partial_fit(X, Y, np.unique(Y))
+
+GaussianNB()
+print(clf_pf.predict([[-0.8, -1]]))
